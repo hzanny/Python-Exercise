1a17b78e3bf6e1a65051a75b5c668dfc018156ee
hzanny <61458713+hzanny@users.noreply.github.com>
2020-03-10 14:59:28 +0200

    Add files via upload

---
+++ Practice1.py
@@ -0,0 +1,29 @@
+strSentence="lifechoices coding academy"
+print(type(strSentence))
+print(strSentence[2:6])
+number1=25
+number2=50.5
+number3=3e+7j
+print(type(number1),type(2))
+mylist=[65,25,125,78,120,33]
+mylist.sort()
+print(mylist)
+mylist.insert(2,37)
+print(mylist)
+mylist.sort(reverse=True)
+print(mylist)
+xdict={"1":"Amy","2":"Tania","3":"Nathan"}
+print(type(xdict))
+#Was fun exploring some functions of pycharm
+
+
+
+
+
+
+
+
+
+
+
+
