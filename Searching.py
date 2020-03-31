5c435f80aff54b888529730cfaa1f260eb717948
hzanny <61458713+hzanny@users.noreply.github.com>
2020-03-31 18:54:24 +0200

    Add files via upload

---
+++ Searching.py
@@ -0,0 +1,10 @@
+def search(list1,key):
+for i in range(len(lost1))
+if key== list1[1]
+print("key element is found")
+break
+else:
+print("element is not found")
+
+list 1=[23,45,54,1,2,3,7,6]
+key=int(input("enter the key element:"))
