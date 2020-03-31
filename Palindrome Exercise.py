2bac88fe3070378b4de10fc42d7e88a7eb8e9752
hzanny <61458713+hzanny@users.noreply.github.com>
2020-03-31 18:51:10 +0200

    Add files via upload

---
+++ Palindrome Exercise.py
@@ -0,0 +1,15 @@
+def is_palindrome(s)
+    if len(s) < 1:
+        return true
+    else:
+        if s[0] == s[-1]
+            return
+is_palindrome(s[1:-1])
+        else:
+        return false
+a=str(input("Enter string:"))
+if(is_palindrome(a)==true):
+    print("string is a palindrome!") 
+ else:
+      print("string isn't a palindrome!")
+                
